53fbb6a165eb696f06a7315fa278877422c72b64
Abdyrakhman Taskibayev <taskibaev.ab@gmail.com>
2023-08-12 18:47:18 +0500

    just some code for start

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('script included');
+
+let screen_width = window.screen.width;
+let screen_avail_width = window.screen.availWidth;
+
+console.log('screen width: ', screen_width);
+console.log('screen available width: ', screen_avail_width);
+
+let x_cell_count = 30;
+let y_cell_count = 30;
+
+let left_margin = 10;
+let right_margin = 10;
+
+function calculateFieldWidth(cellCount, left_margin, right_margin)
+{
+    let margin = left_margin + right_margin;
+
+    let screen_width = window.screen.availWidth;
+
+    
+}
